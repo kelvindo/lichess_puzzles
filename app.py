841189d89abac688a7b3bbcea39e821620fbfc62
Kelvin Do <kdo@plaid.com>
2023-10-29 15:26:31 -0400

    Implement mixed puzzles

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
-from process_puzzles import generate_puzzle_pack_pgn_strings
-from generate_puzzles import PuzzleGenerator
 
-INPUT_FILE = "puzzles/middlegame.csv"
-PUZZLE_PACKS = ["Opening", "Middlegame", "Endgame", "Mixed"]
+from generate_puzzles import (
+    PuzzleGenerator,
+    PUZZLES_OPENING,
+    PUZZLES_MIDDLEGAME,
+    PUZZLES_ENDGAME,
+    PUZZLES_MIXED,
+)
 
 
 @st.cache_resource
@@ -12,12 +15,13 @@ def get_puzzle_generator():
 
 
 puzzle_generator = get_puzzle_generator()
-puzzle_pack = st.selectbox("Puzzle Pack", PUZZLE_PACKS)
+puzzle_pack = st.selectbox(
+    "Puzzle Pack", [PUZZLES_OPENING, PUZZLES_MIDDLEGAME, PUZZLES_ENDGAME, PUZZLES_MIXED]
+)
 num_puzzles = st.slider("Number of Puzzles", 1, 32, 4)
 
 pgn_strings = puzzle_generator.generate_puzzle_pack_pgn_strings(
-    puzzle_pack.lower(),
-    num_puzzles,
+    puzzle_pack, num_puzzles
 )
 
 st.text_area("PGNs", "".join(pgn_strings), height=1000, key="pgn_text_area")
--- generate_puzzles.py
+++ generate_puzzles.py
@@ -2,12 +2,17 @@ import csv
 import random
 from process_puzzles import Puzzle
 from typing import Dict, List
-from dataclasses import dataclass
+
+
+PUZZLES_OPENING = "Opening"
+PUZZLES_MIDDLEGAME = "Middlegame"
+PUZZLES_ENDGAME = "Endgame"
+PUZZLES_MIXED = "Mixed"
 
 PUZZLE_FILES = {
-    "opening": "puzzles/opening.csv",
-    "middlegame": "puzzles/middlegame.csv",
-    "endgame": "puzzles/endgame.csv",
+    PUZZLES_OPENING: "puzzles/opening.csv",
+    PUZZLES_MIDDLEGAME: "puzzles/middlegame.csv",
+    PUZZLES_ENDGAME: "puzzles/endgame.csv",
 }
 
 
@@ -20,17 +25,31 @@ class PuzzleGenerator:
         self.puzzle_mapping = self.load_puzzles()
 
     def load_puzzles(self) -> Dict[str, List[Puzzle]]:
-        puzzle_mapping: Dict[str, List[Puzzle]] = {}
+        puzzle_mapping: Dict[str, List[Puzzle]] = {
+            PUZZLES_OPENING: [],
+            PUZZLES_MIDDLEGAME: [],
+            PUZZLES_ENDGAME: [],
+            PUZZLES_MIXED: [],
+        }
         for name, file_name in PUZZLE_FILES.items():
             with open(file_name, "r") as f:
                 reader = csv.DictReader(f)
                 for row in reader:
                     puzzle = Puzzle.from_dict(row)
-                    puzzle_mapping.setdefault(name, []).append(puzzle)
+                    puzzle_mapping[name].append(puzzle)
+
+        puzzle_mapping[PUZZLES_MIXED] = (
+            puzzle_mapping[PUZZLES_OPENING]
+            + puzzle_mapping[PUZZLES_MIDDLEGAME]
+            + puzzle_mapping[PUZZLES_ENDGAME]
+        )
+
         return puzzle_mapping
 
     def generate_puzzle_pack_pgn_strings(
-        self, puzzle_pack_name, num_puzzles=32
+        self,
+        puzzle_pack_name: str,
+        num_puzzles: int = 32,
     ) -> List[str]:
         pgn_strings = []
         for puzzle in random.sample(self.puzzle_mapping[puzzle_pack_name], num_puzzles):
