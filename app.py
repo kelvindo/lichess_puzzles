da9941eece350814f0b3bf903df264ae0a9bf50d
Kelvin Do <kelvin.h.do@gmail.com>
2023-12-10 21:39:43 -0500

    Tweak UI

--- app.py
+++ app.py
@@ -11,36 +11,6 @@ def get_puzzle_generator():
 
 puzzle_generator = get_puzzle_generator()
 
-puzzle_pack = st.selectbox(
-    "Puzzle Pack",
-    puzzle_generator.get_puzzle_pack_names(),
-)
-
-rating_list = [i for i in range(500, 2100, 100)]
-target_rating = st.selectbox(
-    "Target Rating", rating_list, index=int(len(rating_list) / 2)
-)
-
-# num_puzzles = st.slider("Number of Puzzles", min_value=1, max_value=32, value=4)
-
-fen, analysis_url = puzzle_generator.generate_puzzle_fen_string(
-    puzzle_pack_name=puzzle_pack,
-    target_rating=target_rating,
-)
-
-# pgn_strings = puzzle_generator.generate_puzzle_pack_pgn_strings(
-#     puzzle_pack,
-#     num_puzzles,
-#     target_rating,
-# )
-
-
-# st.text_area("PGNs", "".join(pgn_strings), height=320, key="pgn_text_area")
-
-# st.caption(
-#     "Copy the PGNs above, go to a Lichess study, add chapter, paste the PGNs, and choose 'Orientation: Automatic'"
-# )
-
 # flipped = "w" not in fen
 # board = chess.Board(fen)
 # board_image = chess.svg.board(
@@ -55,9 +25,25 @@ fen, analysis_url = puzzle_generator.generate_puzzle_fen_string(
 # )
 # st.image(image=board_image)
 
-st.components.v1.iframe(analysis_url, width=350, height=625, scrolling=True)
+puzzle_pack = st.selectbox(
+    "Puzzle Pack",
+    puzzle_generator.get_puzzle_pack_names(),
+)
 
-col1, col2 = st.columns([1, 7])
+rating_list = [i for i in range(500, 2100, 100)]
+# target_rating = st.selectbox(
+#     "Target Rating", rating_list, index=int(len(rating_list) / 2)
+# )
+
+fen, analysis_url = puzzle_generator.generate_puzzle_fen_string(
+    puzzle_pack_name=puzzle_pack,
+    target_rating=1400,
+)
+
+st.components.v1.iframe(analysis_url, width=370, height=515, scrolling=False)
+
+
+col1, col2 = st.columns([1, 1])
 
 with col1:
     st.link_button("Analyze", analysis_url)
