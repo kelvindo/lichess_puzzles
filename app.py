1fbff4afcb0eb062b483d66ab1b9d8d0f52aa9de
Kelvin Do <kelvin.h.do@gmail.com>
2023-12-10 22:20:48 -0500

    Generate personalized puzzles

--- app.py
+++ app.py
@@ -11,29 +11,10 @@ def get_puzzle_generator():
 
 puzzle_generator = get_puzzle_generator()
 
-# flipped = "w" not in fen
-# board = chess.Board(fen)
-# board_image = chess.svg.board(
-#     board,
-#     size=600,
-#     flipped=flipped,
-#     borders=False,
-#     colors={
-#         "square light": "#f0d9b5",
-#         "square dark": "#b58863",
-#     },
-# )
-# st.image(image=board_image)
-
 puzzle_pack = st.selectbox(
     "Puzzle Pack",
     puzzle_generator.get_puzzle_pack_names(),
 )
-
-rating_list = [i for i in range(500, 2100, 100)]
-# target_rating = st.selectbox(
-#     "Target Rating", rating_list, index=int(len(rating_list) / 2)
-# )
 
 fen, analysis_url = puzzle_generator.generate_puzzle_fen_string(
     puzzle_pack_name=puzzle_pack,
--- generate_puzzles.py
+++ generate_puzzles.py
@@ -1,13 +1,18 @@
 import csv
 import random
+import io
+import chess.pgn
 from process_puzzles import Puzzle
 from typing import Dict, List, Tuple
+import lichess.api
+from lichess.format import PGN
 
 
 PUZZLES_OPENING = "Opening"
 PUZZLES_MIDDLEGAME = "Middlegame"
 PUZZLES_ENDGAME = "Endgame"
 PUZZLES_RANDOM = "Random"
+PUZZLES_PERSONALIZED = "Personalized"
 
 PUZZLE_FILES = {
     PUZZLES_OPENING: "puzzles/opening.csv",
@@ -16,7 +21,11 @@ PUZZLE_FILES = {
 }
 
 NUM_OPENINGS = 256
+MIN_PUZZLES_FOR_STRUCTURE = 5
 RATING_SAMPLE_SIZE = 1000
+
+LICHESS_USERNAME = "trisolaran3"
+LICHESS_LAST_N_GAMES = 5
 
 
 def coin_flip() -> bool:
@@ -26,7 +35,8 @@ def coin_flip() -> bool:
 class PuzzleGenerator:
     def __init__(self):
         self.puzzle_mapping = self.load_puzzles()
-        self.opening_puzzle_mapping = self.load_opening_puzzles()
+        # self.opening_puzzles_by_name = self.load_opening_puzzles_by_name()
+        self.opening_puzzles_by_structure = self.load_opening_puzzles_by_structure()
 
     def load_puzzles(self) -> Dict[str, List[Puzzle]]:
         puzzle_mapping: Dict[str, List[Puzzle]] = {
@@ -50,8 +60,8 @@ class PuzzleGenerator:
 
         return puzzle_mapping
 
-    def load_opening_puzzles(self) -> Dict[str, List[Puzzle]]:
-        opening_puzzle_mapping: Dict[str, List[Puzzle]] = {}
+    def load_opening_puzzles_by_name(self) -> Dict[str, List[Puzzle]]:
+        opening_puzzles_by_name: Dict[str, List[Puzzle]] = {}
         with open("puzzles/opening_tag.csv", "r") as f:
             reader = csv.DictReader(f)
             for row in reader:
@@ -60,25 +70,56 @@ class PuzzleGenerator:
                     if not tag.strip():
                         continue
                     display_tag = convert_to_display(tag)
-                    opening_puzzle_mapping.setdefault(display_tag, []).append(puzzle)
+                    opening_puzzles_by_name.setdefault(display_tag, []).append(puzzle)
 
         # Filter to the top NUM_OPENINGS openings.
-        opening_puzzle_mapping = {
+        opening_puzzles_by_name = {
             k: v
             for k, v in sorted(
-                opening_puzzle_mapping.items(), key=lambda x: len(x[1]), reverse=True
+                opening_puzzles_by_name.items(), key=lambda x: len(x[1]), reverse=True
             )[:NUM_OPENINGS]
         }
 
         # Sort the openings by name.
-        opening_puzzle_mapping = {
+        opening_puzzles_by_name = {
             k: v
             for k, v in sorted(
-                opening_puzzle_mapping.items(), key=lambda x: x[0], reverse=False
+                opening_puzzles_by_name.items(), key=lambda x: x[0], reverse=False
             )
         }
 
-        return opening_puzzle_mapping
+        return opening_puzzles_by_name
+
+    def load_opening_puzzles_by_structure(self) -> Dict[str, List[Puzzle]]:
+        opening_puzzles_by_structure: Dict[str, List[Puzzle]] = {}
+        with open("puzzles/opening_tag.csv", "r") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                puzzle = Puzzle.from_dict(row)
+                structure1 = pawns_only_fen(puzzle.fen)
+                opening_puzzles_by_structure.setdefault(structure1, []).append(puzzle)
+
+                structure2 = pawns_only_fen(
+                    puzzle.generate_puzzle_position(defensive=False)
+                )
+
+                if structure1 == structure2:
+                    continue
+
+                opening_puzzles_by_structure.setdefault(structure2, []).append(puzzle)
+
+        # Filter to structures with minimum number of puzzles.
+        opening_puzzles_by_structure = {
+            k: v
+            for k, v in sorted(
+                opening_puzzles_by_structure.items(),
+                key=lambda x: len(x[1]),
+                reverse=True,
+            )
+            if len(v) > MIN_PUZZLES_FOR_STRUCTURE
+        }
+
+        return opening_puzzles_by_structure
 
     def get_puzzle_pack_names(self) -> List[str]:
         puzzle_packs = [
@@ -86,9 +127,10 @@ class PuzzleGenerator:
             PUZZLES_OPENING,
             PUZZLES_MIDDLEGAME,
             PUZZLES_ENDGAME,
+            PUZZLES_PERSONALIZED,
         ]
 
-        puzzle_packs.extend(self.opening_puzzle_mapping.keys())
+        # puzzle_packs.extend(self.opening_puzzles_by_name.keys())
         return puzzle_packs
 
     def generate_puzzle_pack_pgn_strings(
@@ -101,8 +143,8 @@ class PuzzleGenerator:
         puzzles = []
         if puzzle_pack_name in self.puzzle_mapping:
             puzzles = self.puzzle_mapping[puzzle_pack_name]
-        elif puzzle_pack_name in self.opening_puzzle_mapping:
-            puzzles = self.opening_puzzle_mapping[puzzle_pack_name]
+        elif puzzle_pack_name in self.opening_puzzles_by_name:
+            puzzles = self.opening_puzzles_by_name[puzzle_pack_name]
         else:
             return ["Invalid puzzle pack name."]
 
@@ -115,6 +157,19 @@ class PuzzleGenerator:
             )
         return pgn_strings
 
+    def get_personalized_puzzles(self) -> List[Puzzle]:
+        structures = get_structure_sets_from_lichess(
+            LICHESS_USERNAME, LICHESS_LAST_N_GAMES
+        )
+
+        puzzles: List[Puzzle] = []
+        for structure in structures:
+            matching_puzzles = self.opening_puzzles_by_structure.get(structure)
+            if matching_puzzles:
+                puzzles.extend(matching_puzzles)
+
+        return puzzles
+
     def generate_puzzle_fen_string(
         self,
         puzzle_pack_name: str,
@@ -123,8 +178,8 @@ class PuzzleGenerator:
         puzzles = []
         if puzzle_pack_name in self.puzzle_mapping:
             puzzles = self.puzzle_mapping[puzzle_pack_name]
-        elif puzzle_pack_name in self.opening_puzzle_mapping:
-            puzzles = self.opening_puzzle_mapping[puzzle_pack_name]
+        elif puzzle_pack_name in self.opening_puzzles_by_name:
+            puzzles = self.opening_puzzles_by_name[puzzle_pack_name]
         else:
             return ["Invalid puzzle pack name."]
 
@@ -159,3 +214,62 @@ def target_puzzles_by_rating(puzzles: List[Puzzle], target_rating: int):
     return sorted(puzzles, key=lambda x: abs(x.rating - target_rating))[
         :RATING_SAMPLE_SIZE
     ]
+
+
+def extract_fens_from_pgn(pgn_string):
+    # Parse the PGN string
+    pgn = chess.pgn.read_game(io.StringIO(pgn_string))
+
+    # List to store FENs
+    fens = []
+
+    # Iterate over all moves and save the FEN after each move
+    board = pgn.board()
+    for move in pgn.mainline_moves():
+        board.push(move)
+        fens.append(board.fen())
+
+    return fens
+
+
+def get_structure_sets_from_pgn(pgn_string):
+    structures = set()
+    fens = extract_fens_from_pgn(pgn_string)
+    for fen in fens:
+        structures.add(pawns_only_fen(fen))
+    return structures
+
+
+def pawns_only_fen(fen):
+    board_layout = fen.split(" ")[0]
+    new_layout = []
+
+    for rank in board_layout.split("/"):
+        new_rank = ""
+        empty_squares = 0
+
+        for char in rank:
+            if char in "Pp":
+                if empty_squares > 0:
+                    new_rank += str(empty_squares)
+                    empty_squares = 0
+                new_rank += char
+            elif char.isdigit():
+                empty_squares += int(char)
+            else:
+                empty_squares += 1
+
+        if empty_squares > 0:
+            new_rank += str(empty_squares)
+
+        new_layout.append(new_rank)
+
+    return "/".join(new_layout)
+
+
+def get_structure_sets_from_lichess(username: str, limit: int) -> List[str]:
+    structures = set()
+    pgns = lichess.api.user_games(username, max=limit, format=PGN)
+    for pgn in pgns:
+        structures.update(get_structure_sets_from_pgn(pgn))
+    return structures
